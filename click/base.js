eae102a5abd5fa54e257f985ba961366a47c066f
Mohamed-Mehdi-Gara <garamehdi57@yahoo.com>
2021-12-05 15:32:27 +0100

    edited base.js file

--- click/base.js
+++ click/base.js
@@ -1,5 +1,8 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
-
+    $("span").on("click", function handleClick(event){
+        var itemText = "You Clicked: " + $(this).text() + " at " + Date.now();
+        $("ul").append("<li>" + itemText + "</li>")
+      })
 })
